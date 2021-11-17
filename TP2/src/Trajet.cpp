b1d85038e92c31a5e1e7be53de53ba35998002e8
JorickPepin <jorick.pepin@gmail.com>
2021-11-17 16:53:13 +0100

    ajout structure premières classes

---
+++ TP2/src/Trajet.cpp
@@ -0,0 +1,74 @@
+/*************************************************************************
+                           Trajet  -  description
+                             -------------------
+    début                : 17/11/2021
+    copyright            : (C) 2021 par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Trajet> (fichier Trajet.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type Trajet::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+Trajet & Trajet::operator = ( const Trajet & unTrajet )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Trajet::Trajet ( const Trajet & unTrajet )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <Trajet>" << endl;
+#endif
+} //----- Fin de Trajet (constructeur de copie)
+
+
+Trajet::Trajet ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Trajet>" << endl;
+#endif
+} //----- Fin de Trajet
+
+
+Trajet::~Trajet ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Trajet>" << endl;
+#endif
+} //----- Fin de ~Trajet
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
---
+++ TP2/src/TrajetSimple.h
@@ -0,0 +1,77 @@
+/*************************************************************************
+                           TrajetSimple  -  description
+                             -------------------
+    début                : 17/11/2021
+    copyright            : (C) 2021 par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----------------
+#if ! defined ( TRAJETSIMPLE_H )
+#define TRAJETSIMPLE_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <TrajetSimple>
+//
+//
+//------------------------------------------------------------------------
+
+class TrajetSimple : public Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    TrajetSimple & operator = ( const TrajetSimple & unTrajetSimple );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    TrajetSimple ( const TrajetSimple & unTrajetSimple );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    TrajetSimple ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~TrajetSimple ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <TrajetSimple>
+
+#endif // TRAJETSIMPLE_H
+
