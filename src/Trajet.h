a24fd43a01d65dcf28c02816c2b18a681f9c4a5d
JorickPepin <jorick.pepin@gmail.com>
2021-11-17 16:53:13 +0100

    ajout structure premières classes

---
+++ src/Trajet.h
@@ -0,0 +1,77 @@
+/*************************************************************************
+                           Trajet  -  description
+                             -------------------
+    début                : 17/11/2021
+    copyright            : (C) 2021 par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Trajet> (fichier Trajet.h) ----------------
+#if ! defined ( TRAJET_H )
+#define TRAJET_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Trajet>
+//
+//
+//------------------------------------------------------------------------
+
+class Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Trajet & operator = ( const Trajet & unTrajet );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Trajet ( const Trajet & unTrajet );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Trajet>
+
+#endif // TRAJET_H
+
---
+++ src/TrajetSimple.cpp
@@ -0,0 +1,74 @@
+/*************************************************************************
+                           TrajetCompose  -  description
+                             -------------------
+    début                : 17/11/2021
+    copyright            : (C) 2021 par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <TrajetCompose> (fichier TrajetCompose.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "TrajetCompose.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type TrajetCompose::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+TrajetCompose & TrajetCompose::operator = ( const TrajetCompose & unTrajetCompose )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+TrajetCompose::TrajetCompose ( const TrajetCompose & unTrajetCompose )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de TrajetCompose (constructeur de copie)
+
+
+TrajetCompose::TrajetCompose ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de TrajetCompose
+
+
+TrajetCompose::~TrajetCompose ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de ~TrajetCompose
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
