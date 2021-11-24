8e91ae764998b60ef5329129fd3084d11910ee75
Leclercq--Cuvelier Ines <inesleclercq@gmail.com>
2021-11-24 15:51:50 +0100

    Réalisation de la classe TrajetSimple

--- TP2/src/modeles/TrajetSimple.h
+++ TP2/src/modeles/TrajetSimple.h
@@ -1,9 +1,9 @@
 /*************************************************************************
-                           TrajetSimple  -  description
+                           TrajetSimple  -  Classe fille de Trajet créant des trajets simples
                              -------------------
     début                : 17/11/2021
-    copyright            : (C) 2021 par $AUTHOR$
-    e-mail               : $EMAIL$
+    copyright            : (C) 2021 par Inès Leclercq--Cuvelier & François Foltet & Jorick Pepin
+    e-mail               : ines.leclercq---cuvelier@insa-lyon.fr & francois.foltet@insa-lyon.fr & jorick.pepin@insa-lyon.fr
 *************************************************************************/
 
 //---- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----
@@ -18,33 +18,37 @@
 
 //------------------------------------------------------------------------
 // Rôle de la classe <TrajetSimple>
-//
-//
+// Classe permettant d'implémenter des trajets simples c'est à dire des trajets comportant :
+//    - Moyen de Transport
+//    - Ville de Départ
+//    - Ville d'Arrivée
 //------------------------------------------------------------------------
 
 class TrajetSimple : public Trajet {
  public:
     //----------------------------------------------------- Méthodes publiques
 
+    void Afficher();
+    // Mode d'emploi : Méthode pour afficher les trajets simples
+
     //------------------------------------------------- Surcharge d'opérateurs
-    TrajetSimple& operator=(const TrajetSimple& trajetSimple_);
+    //TrajetSimple& operator=(const TrajetSimple& trajetSimple_);
     // Mode d'emploi :
     //
     // Contrat :
     //
 
     //-------------------------------------------- Constructeurs - destructeur
-    TrajetSimple(const TrajetSimple& trajetSimple_);
+    //TrajetSimple(const TrajetSimple& trajetSimple_);
     // Mode d'emploi (constructeur de copie) :
     //
     // Contrat :
     //
 
-    TrajetSimple();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
+   
+    TrajetSimple(const char* moyenDeTransport_,const char* villeDepart_,const char* villeArrivee_);
+    // Mode d'emploi : Constructeur du trajet simple, création de l'objet TrajetSimple
+    // Contrat : les chaines de caractères finissent par '\0'
 
     virtual ~TrajetSimple();
     // Mode d'emploi :
@@ -56,6 +60,9 @@ class TrajetSimple : public Trajet {
     //----------------------------------------------------- Méthodes protégées
 
     //----------------------------------------------------- Attributs protégés
+    char* moyenDeTransport;
+    char* villeDepart;
+    char* villeArrivee;
 };
 
 //--------------------------- Autres définitions dépendantes de <TrajetSimple>
