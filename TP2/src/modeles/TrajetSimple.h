84a3cdf57cc3e6745bcf2838c23d02cfbd5c4f6b
JorickPepin <jorick.pepin@gmail.com>
2021-11-27 16:56:53 +0100

    màj structure tests car acutest ne gère pas plusieurs fichiers

--- TP2/src/modeles/TrajetSimple.h
+++ TP2/src/modeles/TrajetSimple.h
@@ -2,8 +2,8 @@
                            TrajetSimple  -  Classe fille de Trajet créant des trajets simples
                              -------------------
     début                : 17/11/2021
-    copyright            : (C) 2021 par Inès Leclercq--Cuvelier & François Foltet & Jorick Pepin
-    e-mail               : ines.leclercq---cuvelier@insa-lyon.fr & francois.foltet@insa-lyon.fr & jorick.pepin@insa-lyon.fr
+    copyright            : (C) 2021 par Inès Leclercq--Cuvelier, François Foltête, Jorick Pepin
+    e-mail               : ines.leclercq---cuvelier@insa-lyon.fr, francois.foltete@insa-lyon.fr, jorick.pepin@insa-lyon.fr
 *************************************************************************/
 
 //---- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----
