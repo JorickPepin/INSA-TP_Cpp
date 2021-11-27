0577353c3cec6c5bde794f79ea2c292a35b36dd0
JorickPepin <jorick.pepin@gmail.com>
2021-11-27 19:06:35 +0100

    amélioration de l'interface

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
                            Interface  -  description
                              -------------------
-    début                : $DATE$
-    copyright            : (C) $YEAR$ par $AUTHOR$
-    e-mail               : $EMAIL$
+    début                : 24/11/2021
+    copyright            : (C) 2021 par Inès Leclercq--Cuvelier, François Foltête, Jorick Pepin
+    e-mail               : ines.leclercq---cuvelier@insa-lyon.fr, francois.foltete@insa-lyon.fr, jorick.pepin@insa-lyon.fr
 *************************************************************************/
 
 //---- Réalisation de la classe <Interface> (fichier Interface.cpp) ------
@@ -20,81 +20,75 @@
 //----------------------------------------------------------------- PUBLIC
 
 //----------------------------------------------------- Méthodes publiques
+void Interface::AfficherBienvenue() {
+    std::cout << "----------------------- Catalogue -----------------------" << std::endl;
+    std::cout << "Bienvenue sur votre catalogue, que souhaitez-vous faire ?" << std::endl;
+}
+
+void Interface::AfficherFin() {
+    std::cout << "\nFin du programme, à bientôt !" << std::endl;
+    std::cout << "---------------------------------------------------------" << std::endl;
+}
+
 void Interface::AfficherMenu() {
-    std::cout << "0: Quitter" << std::endl;
+    std::cout << "\n0: Quitter" << std::endl;
     std::cout << "1: Ajouter un trajet" << std::endl;
-    std::cout << "2: Afficher les trajets" << std::endl;
+    std::cout << "2: Afficher les trajets\n" << std::endl;
+}
+
+void Interface::AfficherMauvaisChoix() {
+    std::cout << "\nVotre choix ne correspond à aucune commande." << std::endl;
 }
 
 void Interface::AjouterTrajet(Liste* catalogue_) {
-    std::cout << "Quel type de trajet souhaitez-vous créer ?" << std::endl;
+    std::cout << "\nQuel type de trajet souhaitez-vous créer ?\n" << std::endl;
     std::cout << "s: Simple" << std::endl;
-    std::cout << "c: Composé" << std::endl << std::endl;
-    std::cout << "q: Retour au menu" << std::endl;
+    std::cout << "c: Composé" << std::endl;
+    std::cout << "q: Retourner au menu\n" << std::endl;
 
     char choix[TAILLE_MAX];
+
+    std::cout << "Choix : ";
     std::cin >> choix;
 
     switch (choix[0]) {
         case 's':
-            AjouterTrajetSimple(catalogue_);
+            ajouterTrajetSimple(catalogue_);
             break;
         case 'c':
-            AjouterTrajetCompose(catalogue_);
+            ajouterTrajetCompose(catalogue_);
             break;
         case 'q':
             break;
         default:
-            std::cout << "pas la bonne commande" << std::endl;
+            AfficherMauvaisChoix();
             break;
     }
 }
 
 void Interface::AfficherTrajets(Liste* catalogue_) {
-    std::cout << "Le catalogue contient les trajets suivants :" << std::endl;
+    std::cout << "\nLe catalogue contient les trajets suivants :" << std::endl;
 
     catalogue_->Afficher();
 }
 
-//------------------------------------------------- Surcharge d'opérateurs
-Interface & Interface::operator=(const Interface& interface_) {
-}
-
-//-------------------------------------------- Constructeurs - destructeur
-Interface::Interface(const Interface& interface_) {
-    #ifdef MAP
-        std::cout << "Appel au constructeur de copie de <Interface>"
-            << std::endl;
-    #endif
-}
-
-Interface::Interface() {
-    #ifdef MAP
-        std::cout << "Appel au constructeur de <Interface>" << std::endl;
-    #endif
-}
-
-Interface::~Interface() {
-    #ifdef MAP
-        std::cout << "Appel au destructeur de <Interface>" << std::endl;
-    #endif
-}
-
 //------------------------------------------------------------------ PRIVE
 
-//----------------------------------------------------- Méthodes protégées
-void Interface::AjouterTrajetSimple(Liste* catalogue_) {
+//------------------------------------------------------- Méthodes privées
+void Interface::ajouterTrajetSimple(Liste* catalogue_) {
     char villeDepart[TAILLE_MAX];
     char villeArrivee[TAILLE_MAX];
     char transport[TAILLE_MAX];
 
-    std::cout << "Ville de départ : ";
+    std::cout << "\nCréation d'un trajet simple" << std::endl;
+
+    std::cout << "- Ville de départ : ";
     std::cin >> villeDepart;
 
-    std::cout << "Ville d'arrivée : ";
+    std::cout << "- Ville d'arrivée : ";
     std::cin >> villeArrivee;
 
-    std::cout << "Moyen de transport : ";
+    std::cout << "- Moyen de transport : ";
     std::cin >> transport;
 
     TrajetSimple* trajet = new TrajetSimple(
@@ -103,6 +97,6 @@ void Interface::AjouterTrajetSimple(Liste* catalogue_) {
     catalogue_->Ajouter(trajet);
 }
 
-void Interface::AjouterTrajetCompose(Liste* catalogue_) {
+void Interface::ajouterTrajetCompose(Liste* catalogue_) {
     // TODO(*): implémenter
 }
